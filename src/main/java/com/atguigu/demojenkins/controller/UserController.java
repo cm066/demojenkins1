72d7c42e656998e287a5b0b95b028a174a01b0e4
wwxxyy <1446525549@qq.com>
2020-12-30 17:58:50 +0800

    这是分支1新增的一个方法

--- src/main/java/com/atguigu/demojenkins/controller/UserController.java
+++ src/main/java/com/atguigu/demojenkins/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.atguigu.demojenkins.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,14 @@ public class UserController {
     public String findAll(){
 
         return "hello";
+    }
+
+    /**
+     * 这是分支新增的方法
+     * @return
+     */
+    @DeleteMapping
+    public String delete(){
+        return "ok";
     }
 }
